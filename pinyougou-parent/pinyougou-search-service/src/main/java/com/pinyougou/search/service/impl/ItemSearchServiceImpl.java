3a0c9e5828c9cc6af08c752debf5f7e6688a3d30
1158229122 <1158229122@qq.com>
2018-12-13 14:20:38 +0800

    完善了分页查询

--- pinyougou-parent/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-parent/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -42,45 +42,77 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     public Map<String, Object> search(Map searchMap)  {
         Map<String, Object> map = new HashMap<String, Object>();
         //获取搜索的条件
-        String search = (String) searchMap.get("search");
-        if(StringUtils.isEmpty(search)){
-            search="*";//如果搜索条件为空,查询所有
+        String keywords = (String) searchMap.get("keywords");
+        if("".equals(keywords)){
+            keywords="*";//如果搜索条件为空,查询所有
+            System.out.println(keywords);
         }
-        SolrQuery query = new SolrQuery("item_keywords:"+search);
+        SolrQuery query = new SolrQuery("item_keywords:"+keywords);
         //获取搜索条件结束
+        
+        //设置过滤条件
+        addFilterSearch(query,searchMap);
+        //过滤条件设置结束
 
         //获取条件查询的列表
-        map.put("rows",getListSearchMap(query,search));
+        map.putAll(getListSearchMap(query,keywords));
 
         //分组查询
 
-        if(!"*".equals(search)){
+        if(!"*".equals(keywords)){
             map.put("category", searchGroup(query));//如果没有输入条件的时候不启用分组查询
             List<String> categoryList = searchGroup(query);
 
             //条件查询获取品牌和规格
-            for (String str : categoryList) {
-                Map brandAndSpec = searchBrandAndSpec(str);
-                String json = null;
-                try {
-                    json = JSON.json(brandAndSpec);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(json);
-                System.out.println(str);
-                if (brandAndSpec.size()>0){
-                    map.putAll(brandAndSpec);
-                }
+            if (categoryList.size()>0){
+                Map brandAndSpec = searchBrandAndSpec(categoryList.get(0));
+                map.putAll(brandAndSpec);
             }
-
-
         }
         return map;
+    }
+
+    //设置用户需要过滤的条件
+    private void addFilterSearch(SolrQuery query,Map searchMap){
+        //设置品牌过滤
+        String brand = (String) searchMap.get("brand");
+        if (!"".equals(brand)){
+            query.addFilterQuery("item_brand:"+brand);
         }
+        //设置分类过滤
+        String category = (String) searchMap.get("category");
+        if(!"".equals(category)){
+            query.addFilterQuery("item_category:"+category);
+        }
+        //设置规格选项
+        Map<String,String> spec = (Map<String,String>) searchMap.get("spec");
+        if(spec!=null){
+            for (Map.Entry<String, String> entry : spec.entrySet()) {
+                query.addFilterQuery("item_spec_"+entry.getKey()+":"+entry.getValue());
+            }
+        }
+        //过滤价格
+        String priceStr = (String) searchMap.get("price");
+        if (!"".equals(priceStr)){
+            String[] price = priceStr.split("-");
+            query.addFilterQuery("item_price:["+price[0]+" TO "+price[1]+"]");
+        }
+        Integer currentPage = (Integer) searchMap.get("currentPage");//获取当前页
+        if (currentPage==null){
+            currentPage = 1;
+        }
+        Integer pageRows = (Integer) searchMap.get("pageRows");
+        if (pageRows==null){
+            pageRows = 20;
+        }
+        query.setStart((currentPage-1)*pageRows);//设置起始索引
+        query.setRows(pageRows);//设置每页的条数
 
 
+    }
 
+
+    /***************** 通过分类获取品牌列表和规格列表 ***************/
     private Map searchBrandAndSpec(String category){
         Map map = new HashMap();
         long templateId = (long) redisTemplate.boundHashOps("itemCat").get(category);
@@ -91,38 +123,38 @@ public class ItemSearchServiceImpl implements ItemSearchService {
             List<Map> specList = (List<Map>) redisTemplate.boundHashOps("specList").get(templateId);
             map.put("specList", specList);
         }
-
-
-
-
         return map;
     }
 
 
 
     //获取条件查询的列表
-    private List<TbItem> getListSearchMap(SolrQuery query, String search) {
+    private Map<String, Object> getListSearchMap(SolrQuery query, String search) {
         List<TbItem> itemList = new ArrayList<TbItem>();
-
+        Map<String, Object> map = new HashMap<String, Object>();
         try {
-
+            /******************** 高亮开始 ************************/
             query.setHighlight(true);//开启高亮
             query.setHighlightSimplePre("<em color=red>");//设置前缀
             query.setHighlightSimplePost("</em>");//设置后缀
             query.addHighlightField("item_title");//添加需要被高亮的域
             QueryResponse queryResponse = solrServer.query(query);
             Map<String, Map<String, List<String>>> highlighting = queryResponse.getHighlighting();//获取到高亮的字段集合
-
+            /******************** 高亮条件设置参数结束 ************************/
             SolrDocumentList results = queryResponse.getResults();//获取到所有的元素结果集合
+
             for (SolrDocument document : results) {
                 String item_id = (String) document.get("id");
 
+                /******************** 设置高亮的标题 ************************/
                 String item_title = null;
                 if (highlighting!=null&&highlighting.get(item_id)!=null&&highlighting.get(item_id).get("item_title")!=null&&!"*".equals(search)){
                     item_title = highlighting.get(item_id).get("item_title").get(0);
                 }else {
                     item_title = (String) document.get("item_title");
                 }
+                /******************** 高亮标题设置借宿 ************************/
+
                 Double item_price = (Double) document.get("item_price");
                 String item_image = (String) document.get("item_image");
                 Long item_goodsid = (Long) document.get("item_goodsid");
@@ -138,12 +170,16 @@ public class ItemSearchServiceImpl implements ItemSearchService {
                 item.setGoodsId(item_goodsid);
                 item.setCategory(item_category);
                 itemList.add(item);
-            }
 
+            }
+            long numFound = results.getNumFound();
+            map.put("totalPage",numFound);
+            map.put("rows",itemList );
         } catch (SolrServerException e) {
             e.printStackTrace();
         }
-        return itemList;
+
+        return map;
     }
 
     //分组查询
--- pinyougou-parent/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-parent/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,11 +1,147 @@
 app.controller("searchController",function ($scope,searchService) {
     $scope.rowsList = {};//返回的数据
-    $scope.searchMap= {};
-
+    $scope.searchMap= {"keywords":"","brand":"","category":"","price" : '',"currentPage":'1',"pageRows":'20',spec:{}};
+    
+    //主线查询
+    function isNum(a){
+        var regexp =  /^[0-9]+.?[0-9]*$/;
+        return regexp.test(a);
+    }
+    //去哪页
+    $scope.goToPage = function(toPage){
+        $scope.searchMap.currentPage = toPage;
+        $scope.search();
+    }
+    //搜索,所有的方法都走这条主线
     $scope.search = function(){
+
+        $scope.searchMap.currentPage = parseInt($scope.searchMap.currentPage);
+        $scope.searchMap.pageRows = parseInt($scope.searchMap.pageRows);
+        $scope.toPage = $scope.searchMap.currentPage;//每次收索完显示前台
+        if($scope.searchMap.currentPage>$scope.page){
+            $scope.searchMap.currentPage = $scope.page;//如果当前页数超过总页数相等总页数
+        }
+        if($scope.searchMap.currentPage<1){
+            $scope.searchMap.currentPage = 1;//当前页数相等于1//优化参数
+        }
+
+        if(!isNum($scope.searchMap.currentPage)){
+
+            $scope.searchMap.currentPage = 1;//当前页数相等于1//优化参数如果是字符串当前页数就是等于1
+        }
+
+
         searchService.search($scope.searchMap).success(function (response) {
             $scope.resultMap = response;
+            $scope.buildLimitPage();
+            window.scrollTo(0,200);
         })
     }
+    
+    $scope.buildSearchItem = function (key,value) {
+        if (key == "category" || key == "brand"||key == "price"||key == "currentPage"){//不是分类就是品牌
+            $scope.searchMap[key] = value;
+        } else {//否者就是规格选项
+            $scope.searchMap.spec[key] = value;
+        }
+        //添加条件查询
+        $scope.search();
+    }
+    //删除key和value
+    $scope.removeSearch = function (key,value) {
+        if (key == "category" || key == "brand"||key=='price'){//不是分类就是品牌
+            $scope.searchMap[key] = "";
+        } else {//否者就是规格选项
+            delete $scope.searchMap.spec[key];
+        }
+        $scope.search();
+    }
+
+
+    $scope.buildLimitPage = function () {
+        $scope.firstDot = true;//前面有点
+        $scope.lastFot = true;//后面有点
+        var startPage ;//开始页
+        var endPage;//结束页
+        $scope.limitPage = [];
+        var currentPage = parseInt($scope.searchMap.currentPage);//当前页面
+        $scope.searchMap.pageRows = parseInt($scope.searchMap.pageRows);
+        $scope.page = $scope.resultMap.totalPage/$scope.searchMap.pageRows;//总页数
+        $scope.page = Math.ceil($scope.page);//总页数
+
+        //总页面不足5
+        if ($scope.page<=5) {
+            startPage = 1;
+            endPage = $scope.page;
+            $scope.firstDot = false;//前面无点
+            $scope.lastFot = false;//后面无点
+        }else {
+            //总页面超过5个
+            startPage = currentPage -2;
+            endPage =currentPage+2;
+
+
+            if (startPage<=1){
+                //前面不足2个
+                startPage =1;
+                endPage = 5;
+                $scope.firstDot = false;//前面无点
+            }
+            if (endPage >=$scope.page ) {
+                //后面不足2个
+                startPage = $scope.page-4;
+                endPage = $scope.page
+                $scope.lastFot = false;//后面无点
+            }
+
+        }
+
+
+        for(var i =startPage;i<=endPage ;i++){
+            $scope.limitPage.push(i);
+        }
+    }
+    //是否相同页
+    $scope.isActive = function(item){
+        if ($scope.searchMap.currentPage==item) {
+            return true;
+        }
+        return false;
+    }
+
+    //是否是第一页
+    $scope.isfirstPage = function () {
+
+        if ($scope.searchMap.currentPage==1) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    //是否最后一页
+    $scope.islastPage = function () {
+
+        if ($scope.page==$scope.searchMap.currentPage) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    $scope.queryByPage = function (currentPage) {
+
+        //页码验证
+        if(currentPage<1 || currentPage>$scope.resultMap.page){
+            return;}
+
+        $scope.searchMap.currentPage=currentPage;
+        $scope.search();
+    }
+
+
+
 
 })
